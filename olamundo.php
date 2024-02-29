d78b4e7b977ac1aba791c5725a6eeaa9ac557741
rodhes <rodhesc@hotmail.com>
2024-02-29 20:11:33 -0300

    iniciando curso php

---
+++ olamundo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$num=6;
+$num2=7;
+$total=$num+$num2;
+echo"$total";
+?>
+</body>
+</html>
